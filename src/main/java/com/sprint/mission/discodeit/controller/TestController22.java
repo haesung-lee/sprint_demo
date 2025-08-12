846d2b2e584b1d531bcb65bd4975d85a487392bd
haesung-lee <haesung7.lee@gmail.com>
2025-08-12 14:16:33 +0900

    feat : add TestController22, modify ~~~

---
+++ src/main/java/com/sprint/mission/discodeit/controller/TestController22.java
@@ -0,0 +1,8 @@
+package com.sprint.mission.discodeit.controller;
+
+public class TestController22 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
